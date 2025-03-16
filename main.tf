f9052d1893118bfd8182a63c7d16684b552f6f90
Vuntla Divya <vuntladivya.jaya@gmail.com>
2025-03-16 22:43:26 +0530

    test

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "aws_instance" "my_instance" {
     instance_type = var.instance_type
     key_name = var.key_name
     vpc_security_group_ids = [aws_security_group.my_sg.id]
+    iam_instance_profile = aws_iam_role.instance_role.name
     tags = var.tags
 }
 
@@ -33,4 +34,55 @@ resource "aws_security_group" "my_sg" {
         cidr_blocks = var.cidr_blocks
     }
     tags = var.tags
+}
+
+resource "aws_iam_role" "instance_role" {
+  name = "${var.project}-${var.env}-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = var.tags
+  
+}
+resource "aws_iam_policy" "instance_s3_policy" {
+  name        = "${var.project}-${var.env}-policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListMyBucket"
+        ]
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.my_bucket.arn}/*"
+      },
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "s3policy_attachment_role"{
+
+
+  role       = aws_iam_role.instance_role.name
+  policy_arn = aws_iam_policy.instance_s3_policy.arn
 }
